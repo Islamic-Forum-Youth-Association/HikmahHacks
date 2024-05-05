86f29955c0ec3935554032802eafd5335567eefb
Raeid <raeidus15@gmail.com>
2024-05-05 00:03:27 -0400

    changed hamburger modal to a sidebar

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,45 +1,31 @@
-'use client';
-import React, { useState } from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import { IoTerminal } from "react-icons/io5";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { motion, useAnimation } from "framer-motion";
 
 export default function Nav() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const controls = useAnimation();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [scrollToSection, setScrollToSection] = useState(null);
 
   const handleScroll = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    setScrollToSection(id);
+    setIsSidebarOpen(false); // Close the sidebar
+  }
+
+  useEffect(() => {
+    if (scrollToSection) {
+      const section = document.getElementById(scrollToSection);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+      setScrollToSection(null); // Reset scrollToSection after scrolling
     }
+  }, [scrollToSection]);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
   }
-
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  }
-
-  // Animation 
-  const modalVariants = {
-    hidden: {
-      x: '100%',
-      opacity: 0,
-    },
-    visible: {
-      x: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        ease: "easeInOut",
-      },
-    },
-  };
-
-  // Animation controls
-  const toggleModalAnimation = async () => {
-    await controls.start(isModalOpen ? "hidden" : "visible");
-    toggleModal();
-  };
 
   return (
     <nav className='bg-[#202127] p-6 w-screen sticky top-0 z-50 overflow-clip'>
@@ -58,36 +44,42 @@ export default function Nav() {
             <li className='rounded-full bg-[#676767] py-5 -translate-y-5'><button onClick={() => handleScroll('register')} className='flex text-[#4FFFA0] -translate-y-1'><b>REGISTER</b></button></li>
           </ul>
 
-        {/* Modal Toggle Button */}
-        <button onClick={toggleModalAnimation} className="block xl:hidden text-[#676767] p-4 -translate-y-1 scale-150 mr-2">
+        {/* Sidebar Toggle Button */}
+        <button onClick={toggleSidebar} className="block xl:hidden text-[#676767] p-4 -translate-y-1 scale-150 mr-2">
           <GiHamburgerMenu size={24} />
         </button>
       </div>
 
-      {/* Modal Component */}
-      <motion.div 
-        className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center"
-        onClick={toggleModalAnimation}
-        initial="hidden"
-        animate={controls}
-        variants={modalVariants}
-      >
+      {/* Darkened Overlay */}
+      {isSidebarOpen && (
         <motion.div 
-          className="bg-[#202127] w-full max-w-xl mx-auto p-8 rounded-lg text-center"
-          onClick={(e) => e.stopPropagation()} 
-        >
-          <ul className="text-white text-lg space-y-4">
-            <li onClick={() => { handleScroll('home'); toggleModalAnimation(); }}>HOME</li>
-            <li onClick={() => { handleScroll('about'); toggleModalAnimation(); }}>ABOUT</li>
-            <li onClick={() => { handleScroll('faq'); toggleModalAnimation(); }}>FAQ</li>
-            <li onClick={() => { handleScroll('team'); toggleModalAnimation(); }}>MEET THE TEAM</li>
-            <li className="rounded-full bg-[#676767] py-2 px-4 text-[#4FFFA0] inline-block">
-              <button onClick={() => { handleScroll('register'); toggleModalAnimation(); }} className="focus:outline-none">
-                <b>REGISTER</b>
-              </button>
-            </li>
-          </ul>
-        </motion.div>
+          className="fixed inset-0 bg-black bg-opacity-60 z-50"
+          onClick={() => setIsSidebarOpen(false)}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        ></motion.div>
+      )}
+
+      {/* Sidebar Component */}
+      <motion.div 
+        className={`fixed inset-y-0 right-0 bg-[#202127] w-[100%] p-8 z-50 flex flex-col justify-center items-start`}
+        initial={{ x: '100%' }}
+        animate={{ x: isSidebarOpen ? '75%' : '100%' }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <ul className="text-white text-lg space-y-4">
+          <li onClick={() => { handleScroll('home'); }}>HOME</li>
+          <li onClick={() => { handleScroll('about'); }}>ABOUT</li>
+          <li onClick={() => { handleScroll('faq'); }}>FAQ</li>
+          <li onClick={() => { handleScroll('team'); }}>MEET THE TEAM</li>
+          <li className="rounded-full bg-[#676767] py-2 px-4 text-[#4FFFA0] inline-block">
+            <button onClick={() => { handleScroll('register'); }} className="focus:outline-none">
+              <b>REGISTER</b>
+            </button>
+          </li>
+        </ul>
       </motion.div>
     </nav>
   )
