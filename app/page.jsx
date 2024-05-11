b8a2f9359602fd4741c3dc10e060dd9f1128263f
Ammar Hussein <ahusse45@uwo.ca>
2024-05-11 15:50:06 -0400

    Basic animation for hero section

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import Faqs from "./components/faqs";
 import Team_strip from "./components/team";
 import { Ubuntu } from 'next/font/google';
 import Footer from "./components/footer";
+
 export default function Home() {
   return (
     <>
@@ -14,8 +15,9 @@ export default function Home() {
       <Nav/> 
       <Hero/>
       <img className="bg-[#26272D] object-contain scale-150  z-3 background-transparent w-[100%]   overflow-hidden fill-[#202127]" src="/mosque.png" alt="Divider" />
-
+      
       <Faqs/>
+
       <img className="bg-[#26272D] object-contain scale-150  z-3 background-transparent w-[100%]   overflow-hidden fill-[#202127]" src="/wavesOpacity.svg" alt="Divider" />
 
       <Team_strip/>
