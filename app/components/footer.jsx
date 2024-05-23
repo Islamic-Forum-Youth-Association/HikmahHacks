4615e8433e09dbdfcc5f2903574eae9773f284e5
Raeid Usmanali <105375237+Raeid-U@users.noreply.github.com>
2024-05-23 16:02:52 -0400

    revamp to faq (#14)
    * update
    
    work in progress with the transitioning of components in page.jsx
    
    Using a button to cycle as of now.
    
    * update to faq
    
    [WIP]
    revamped faq section to be in an acordian style. some more timing work needed for the animations.
    
    moved the dividers from the page to make working easier
    
    added some padding to the footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,36 +9,26 @@ function Footer() {
     // User state (If necessary) goes here
     return (
         <>
-        {/* JSX goes here.*/}
-        <div id="contact" className="bg-[#1C1D22] z-10">
-        <h1 className="text-5xl text-center text-gray-50 font-bold">Got any questions? Reach out!</h1>
+            {/* JSX goes here.*/}
+            <div id="contact" className="bg-[#1C1D22] z-10">
+                <h1 className="text-5xl text-center text-gray-50 mt-20 font-bold">Got any questions? Reach out!</h1>
+                <div className="container mx-auto px-[30vem] min-h-[90vh] xl:min-h-[50vh] -z-10">
+                    <div className="grid grid-cols-1 gap-32 p-10">
+                        {/* Grid Item 1 */}
+                        <div className="p-4 rounded-md text-center">
+                            <p className="text-xl mt-2.5 text-[#4FFFA0]"><a href="mailto:hikmahhacks@gmail.com">hikmahhacks@gmail.com</a></p>
 
-        <div className="container mx-auto px-[30vem] min-h-[90vh] xl:min-h-[50vh] -z-10">
-            <div className="grid grid-cols-1  gap-32  p-20">
-                {/* Grid Item 1 */}
-                <div className="p-4 rounded-md  text-center ">
-                <p className="text-xl mt-2.5 text-[#4FFFA0]"><a href="mailto:hikmahhacks@gmail.com">hikmahhacks@gmail.com</a></p>
-
-                    {/* Social Media Icons */}
-                    <div className="py-4  flex flex-row [&>*]:px-2 justify-center">
-                        <a href="DiscordLink"><button><RxDiscordLogo size={64} className="inline-block rounded-full mr-2 " color="#D1D5DB"/></button></a>
-
-                        <a href="http://instagram.com/hikmahhacks"><button><FaInstagram size={64} className="inline-block rounded-full" color="#D1D5DB"/></button></a>
-
-
-                        <a href="https://twitter.com/hikmahhacks"><button><FaXTwitter size={64} className="inline-block rounded-full " color="#D1D5DB"/></button></a>
-
-                      </div>
-                
+                            {/* Social Media Icons */}
+                            <div className="py-4 flex flex-row [&>*]:px-2 justify-center">
+                                <a href="DiscordLink"><button><RxDiscordLogo size={64} className="inline-block mr-2" color="#D1D5DB"/></button></a>
+                                <a href="http://instagram.com/hikmahhacks"><button><FaInstagram size={64} className="inline-block " color="#D1D5DB"/></button></a>
+                                <a href="https://twitter.com/hikmahhacks"><button><FaXTwitter size={64} className="inline-block " color="#D1D5DB"/></button></a>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="text-center text-white">&#169; Islamic Forum of Canada {new Date().getFullYear()}</p>
                 </div>
-
-                
             </div>
-            <p className="text-center text-white">&#169; Islamic Forum of Canada {new Date().getFullYear()}</p>
-
-        </div>
-
-    </div>
         </>
     );
 }
